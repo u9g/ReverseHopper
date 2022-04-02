427dbfe8495c5fd6ef44fe1439926be2cfd95a92
u9g <43508353+u9g@users.noreply.github.com>
2022-04-02 15:31:35 -0400

    add support for crafting a reverse hopper with a barrel or a chest

--- src/main/java/dev/u9g/reversehopper/HopperRecipe.java
+++ src/main/java/dev/u9g/reversehopper/HopperRecipe.java
@@ -2,15 +2,26 @@ package dev.u9g.reversehopper;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ShapedRecipe;
+
+import java.util.Objects;
 
 public class HopperRecipe {
     public static void register() {
-        Bukkit.addRecipe(new ShapedRecipe(Constants.key, Constants.item)
+        var chest = Objects.requireNonNull(NamespacedKey.fromString("reverse_hoppers:recipe_with_chest"));
+        var barrel = Objects.requireNonNull(NamespacedKey.fromString("reverse_hoppers:recipe_with_barrel"));
+        Bukkit.addRecipe(new ShapedRecipe(chest, Constants.item)
                 .shape( "X X",
                         "XYX",
                         " X ")
                 .setIngredient('X', Material.COPPER_INGOT)
                 .setIngredient('Y', Material.CHEST));
+        Bukkit.addRecipe(new ShapedRecipe(barrel, Constants.item)
+                .shape( "X X",
+                        "XYX",
+                        " X ")
+                .setIngredient('X', Material.COPPER_INGOT)
+                .setIngredient('Y', Material.BARREL));
     }
 }
